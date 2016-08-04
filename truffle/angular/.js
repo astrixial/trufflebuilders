060ddc4adbaf617451d8b56cfc4a92585b2fd547
astrixial <bitcoinplus2@gmail.com>
2016-08-03 23:00:49 -0500

    Create .js

---
+++ truffle/angular/.js
@@ -0,0 +1,11 @@
+ module.exports = {
+   build: { 
+   "index.html": "index.html",
+   "app.js": [
+    "javascripts/app.js"
+    ],
+    "metaCoin.js": [
+    "javascripts/_vendor/angular.js",
+    "javascripts/metaCoinController.js"
+    ],
+    ....
